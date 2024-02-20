a5db23bfb4c6578488af32d82138e27245a64598
OscarMontilla <120601904+OscarMontilla@users.noreply.github.com>
2024-02-20 13:43:34 +0100

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { panel } from "../componentes/panel";
-import { ModeloPieza } from "../funciones/clases";
-
-// panel.nuevaPieza = panel.crearNuevaPieza()
-panel.crearNuevaPieza()
-
-// Insertamos la pieza en la matriz del panel
-panel.insertarPieza();
-
