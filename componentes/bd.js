799fd205d8db53243a419256f9d0cf1d381991b3
OscarMontilla <120601904+OscarMontilla@users.noreply.github.com>
2024-02-21 13:18:19 +0100

    Add files via upload

---
+++ componentes/bd.js
@@ -0,0 +1,23 @@
+export const juegos = [
+    {
+        avatar: "/account-avatar-profile-user-13-svgrepo-com.svg",
+        nick: "Eric",
+        puntos: "455",
+        fecha:"31/12/2004",
+    },
+
+    {
+        avatar: "/account-avatar-profile-user-13-svgrepo-com.svg",
+        nick: "David",
+        puntos: "2",
+        fecha:"30/05/2004",
+    },
+
+    {
+        avatar: "/account-avatar-profile-user-13-svgrepo-com.svg",
+        nick: "Oscar",
+        puntos: "345",
+        fecha:"04/12/2004",
+    }
+
+];
